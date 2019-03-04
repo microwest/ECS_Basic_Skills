c9bd1181064bf5163e12759cecd26bc21ad61af7
microwest <microwest@163.com>
2019-03-04 12:09:03 +0800

    add two samples: 01ECS_Spawn and 02ECS_Sys_Switch.

---
+++ Skills/Assets/02ECS_Sys_Switch/BallMoveSpeedProxy02.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Entities;
+
+
+[Serializable]
+public struct BallMoveSpeed02:IComponentData
+{
+    public float Value;
+}
+
+[UnityEngine.DisallowMultipleComponent]
+public class BallMoveSpeedProxy02 :ComponentDataProxy<BallMoveSpeed02> { }
